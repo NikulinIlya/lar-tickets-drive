ff0f30bd0fd339fed17c7077db3f9793b1515f0f
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-12 15:57:38 +0300

    test(Unit): make concert test file, add test that checks getting formatted concert date

---
+++ tests/Unit/ConcertTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Concert;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ConcertTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function can_get_formatted_date()
+    {
+        $concert = factory(Concert::class)->create([
+            'date' => Carbon::parse('2016-12-01 8:00pm')
+        ]);
+
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
+}
