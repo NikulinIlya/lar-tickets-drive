550549ad6bb9897d2e25ecdbc7502185f036c9bf
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-13 22:39:58 +0300

    feat(tickets): make and set model, migration

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\features;
 
+use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentGateway;
 use App\Concert;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -16,20 +18,24 @@ class PurchaseTicketsTest extends TestCase
     function customer_can_purchase_concert_tickets()
     {
         $paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $paymentGateway);
+
         $concert = factory(Concert::class)->create(['ticket_price' => 3250]);
 
-        $this->json('POST', "/concerts/{$concert->id}/orders", [
+        $response = $this->json('POST', "/concerts/{$concert->id}/orders", [
             'email' => 'john@example.com',
             'ticket_quantity' => 3,
             'payment_token' => $paymentGateway->getValidTestToken(),
         ]);
+
+        $response->assertStatus(201);
 
         $this->assertEquals(9750, $paymentGateway->totalCharges());
 
         $order = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
 
-        $this->assertEquals(3, $order->tickets->count());
+        $this->assertEquals(3, $order->tickets()->count());
     }
 
 
