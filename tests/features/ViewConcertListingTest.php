f00cf9fcfba3183863ec8f5cce728c26fe604555
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-13 17:31:45 +0300

    feat(COncert): add local scope published

--- tests/features/ViewConcertListingTest.php
+++ tests/features/ViewConcertListingTest.php
@@ -14,7 +14,7 @@ class ViewConcertListingTest extends TestCase
     use DatabaseMigrations;
 
     /** @test */
-    function user_can_view_a_concert_listing()
+    function user_can_view_a_published_concert_listing()
     {
         $concert = Concert::create([
             'title' => 'The Red Chord',
@@ -27,6 +27,7 @@ class ViewConcertListingTest extends TestCase
             'state' => 'ON',
             'zip' => '17916',
             'additional_information' => 'For tickets, call (555) 555-5555.',
+            'published_at' => Carbon::parse('-1 week')
         ]);
 
         $response = $this->get('/concerts/'.$concert->id);
