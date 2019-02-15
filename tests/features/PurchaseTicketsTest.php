550c565338dede8a12f90cea893defa9d2f6ffa7
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-15 20:14:45 +0300

    test(PurchaseTicketsTest): make test that checks that an order is not created if payment fails

--- tests/features/PurchaseTicketsTest.php
+++ tests/features/PurchaseTicketsTest.php
@@ -52,6 +52,22 @@ class PurchaseTicketsTest extends TestCase
     }
 
     /** @test */
+    function an_order_is_not_created_if_payment_fails()
+    {
+        $concert = factory(Concert::class)->create();
+
+        $response = $this->orderTickets($concert, [
+            'email' => 'john@example.com',
+            'ticket_quantity' => 3,
+            'payment_token' => 'invalid-payment-token',
+        ]);
+
+        $response->assertStatus(422);
+        $order = $concert->orders()->where('email', 'john@example.com')->first();
+        $this->assertNull($order);
+    }
+
+    /** @test */
     function email_is_required_to_purchase_tickets()
     {
         $concert = factory(Concert::class)->create();
@@ -117,6 +133,4 @@ class PurchaseTicketsTest extends TestCase
 
         $this->assertValidationError($response, 'payment_token');
     }
-
-
 }
