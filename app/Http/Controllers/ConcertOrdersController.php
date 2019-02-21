9082f06a8592366317ccbddc2a9c8228a62ebd0f
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-21 18:38:19 +0300

    feat(app): make Reservation class, change ConcertOrdersController with totalCost function

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -7,6 +7,7 @@ use App\Billing\PaymentGateway;
 use App\Concert;
 use App\Exceptions\NotEnoughTicketsException;
 use App\Order;
+use App\Reservation;
 use Illuminate\Http\Request;
 
 class ConcertOrdersController extends Controller
@@ -30,9 +31,11 @@ class ConcertOrdersController extends Controller
 
         try {
             $tickets = $concert->findTickets(request('ticket_quantity'));
-            $this->paymentGateway->charge($tickets->sum('price'), request('payment_token'));
+            $reservation = new Reservation($tickets);
 
-            $order = Order::forTickets($tickets, request('email'), $tickets->sum('price'));
+            $this->paymentGateway->charge($reservation->totalCost(), request('payment_token'));
+
+            $order = Order::forTickets($tickets, request('email'), $reservation->totalCost());
 
             return response()->json($order, 201);
         } catch (PaymentFailedException $e) {
