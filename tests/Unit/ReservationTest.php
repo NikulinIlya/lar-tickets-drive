5dea0cf44c2f79fadb489f948bdce55da05274b2
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-21 18:39:55 +0300

    refactor(ReservationTest): remove using database in the test

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -2,21 +2,19 @@
 
 namespace Tests\Unit;
 
-use App\Concert;
 use App\Reservation;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ReservationTest extends TestCase
 {
-    use DatabaseMigrations;
-
     /** @test */
     function calculating_the_total_cost()
     {
-        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
-        $tickets = $concert->findTickets(3);
+        $tickets = collect([
+            (object)['price' => 1200],
+            (object)['price' => 1200],
+            (object)['price' => 1200],
+        ]);
 
         $reservation = new Reservation($tickets);
 
