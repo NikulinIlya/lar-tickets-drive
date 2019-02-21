afc147f6af1fbef0aad673a39e27b46113663f45
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-21 23:03:20 +0300

    feat(FakePaymentGateway): add function beforeFirstCharge

--- app/Billing/FakePaymentGateway.php
+++ app/Billing/FakePaymentGateway.php
@@ -5,6 +5,7 @@ namespace App\Billing;
 class FakePaymentGateway implements PaymentGateway
 {
     private $charges;
+    private $beforeFirstChargeCallback;
 
     public function __construct()
     {
@@ -18,7 +19,13 @@ class FakePaymentGateway implements PaymentGateway
 
     public function charge($amount, $token)
     {
-        if($token !== $this->getValidTestToken()) {
+        if ($this->beforeFirstChargeCallback !== null) {
+            $callback = $this->beforeFirstChargeCallback;
+            $this->beforeFirstChargeCallback = null;
+            $callback($this);
+        }
+
+        if ($token !== $this->getValidTestToken()) {
             throw new PaymentFailedException;
         }
         $this->charges[] = $amount;
@@ -27,5 +34,10 @@ class FakePaymentGateway implements PaymentGateway
     public function totalCharges()
     {
         return $this->charges->sum();
+    }
+
+    public function beforeFirstCharge($callback)
+    {
+        $this->beforeFirstChargeCallback = $callback;
     }
 }
