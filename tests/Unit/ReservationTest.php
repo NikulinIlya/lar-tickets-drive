f06fa6b3206ab6ae41e285e7dc3a7e9901053b35
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-21 18:36:23 +0300

    test(Unit): make ReservationTest

---
+++ tests/Unit/ReservationTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Concert;
+use App\Reservation;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReservationTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function calculating_the_total_cost()
+    {
+        $concert = factory(Concert::class)->create(['ticket_price' => 1200])->addTickets(3);
+        $tickets = $concert->findTickets(3);
+
+        $reservation = new Reservation($tickets);
+
+        $this->assertEquals(3600, $reservation->totalCost());
+    }
+
+}
