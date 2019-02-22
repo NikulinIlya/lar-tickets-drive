f057e8cd7ba35d11938d4b988b5548efbba47ad9
NikulinIlya <ilia.nikulin@gmail.com>
2019-02-22 22:48:32 +0300

    test(TicketTest): make test a_ticket_can_be_reserved

--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Concert;
 use App\Order;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,6 +12,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class TicketTest extends TestCase
 {
     use DatabaseMigrations;
+
+    /** @test */
+    function a_ticket_can_be_reserved()
+    {
+        $ticket = factory(Ticket::class)->create();
+        $this->assertNull($ticket->reserved_at);
+
+        $ticket->reserve();
+
+        $this->assertNotNull($ticket->fresh()->reserved_at);
+    }
 
     /** @test */
     function a_ticket_can_be_released()
