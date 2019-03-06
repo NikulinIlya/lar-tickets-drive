a162e6fbfa50fc077fdabcd43b59250e3a0ff224
NikulinIlya <ilia.nikulin@gmail.com>
2019-03-06 22:29:12 +0300

    test(ReservationTest): make test reserved_tickets_are_released_when_a_reservation_cancelled

--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Unit;
 
 use App\Reservation;
+use App\Ticket;
+use Mockery;
 use Tests\TestCase;
 
 class ReservationTest extends TestCase
@@ -21,4 +23,22 @@ class ReservationTest extends TestCase
         $this->assertEquals(3600, $reservation->totalCost());
     }
 
+    /** @test */
+    function reserved_tickets_are_released_when_a_reservation_cancelled()
+    {
+        $ticket1 = Mockery::mock(Ticket::class);
+        $ticket1->shouldReceive('release')->once();
+
+        $ticket2 = Mockery::mock(Ticket::class);
+        $ticket2->shouldReceive('release')->once();
+
+        $ticket3 = Mockery::mock(Ticket::class);
+        $ticket3->shouldReceive('release')->once();
+
+        $tickets = collect([$ticket1, $ticket2, $ticket3]);
+        $reservation = new Reservation($tickets);
+
+        $reservation->cancel();
+
+    }
 }
